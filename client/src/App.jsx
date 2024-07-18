082bcfa7a71c28c3a4a2de359cb59518169b2f6c
AuroBhuyan <aurobhuyan123@gmail.com>
2024-07-18 22:13:20 +0530

    corrected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
+import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import {Toaster} from "sonner";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
