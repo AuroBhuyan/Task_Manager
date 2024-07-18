5c10f8fcb01a18050c6e1d833d5475bfc8250289
Khushi-b08 <khushikumaribnb@gmail.com>
2024-07-18 21:03:05 +0530

    c

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,33 @@
+import { Navigate, Outlet, Route, Routes, useLocation } from "react-router-dom";
+import {Toaster} from "sonner";
+import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
-import { Routes, Route, Navigate, Outlet, useLocation} from "react-router-dom";
+import TaskDetails from "./pages/TaskDetails";
+import Trash from "./pages/Trash";
 import Users from "./pages/Users";
-import Dashboard from "./pages/Dashboard.JSX";
+import Tasks from "./pages/Tasks";
 
+function Layout (){
+  const user= ""
+
+  const location=useLocation()
+   return user ? (
+      <div className="w-full h-screen flex-col md:flex-row">
+        <div className="w-1/5 h-screen bg-white sticky top-0 hidden md:block">
+          {/* <Sidebar /> */}
+        </div>
+        {/* <MobileSidebar /> */}
+        <div className="flex-1 overflow-y-auto">
+          {/* <Navbar/> */}
+          <div className="p-4 2x1:px-10">
+            <Outlet />
+          </div>
+        </div>
+      </div>
+   ): (
+    <Navigate to="/log-in" state={{from: location}} replace />
+   )
+}
 function App() {
   return( 
     <main className='w-full min-h-screen bg-[#f3f4f6]'>
@@ -10,7 +35,7 @@ function App() {
         <Route element={<layout />}>
           <Route path="/" element={<Navigate to="/dashboard" />} />
           <Route path="/dashboard >" element={<Dashboard />} />
-          <Route path="/tasks >" element={<Tasks />} />
+          <Route path='/tasks' element={<Tasks />} />
           <Route path="/completed/:status >" element={<Tasks />} />
           <Route path="/in-progress/:status >" element={<Tasks />} />
           <Route path="/todo/:status >" element={<Tasks />} />
@@ -20,6 +45,8 @@ function App() {
         </Route>
         <Route path="/log-in" element={<Login />} />
     </Routes>
+
+    <Toaster richColors />
     </main>
   )
 }
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import {useForm} from "react-hook-form"
 const Login = () => {
+  const user=""
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  }=useForm();
+
+  useEffect(()=> {
+    user && navigate("/dashboard");
+  }, [user]);
+
   return( 
-    <div>
+    <div className="w-full min-h-screen flex items-center justify-center flex-col lg:flex-row bg-[#f3f4f6]">
+      <div className="w-full md:w-auto flex gap-0 md:flex-row items-center justify-center">
+        {/* left side */}
+        <div className="h-full w-full lg:w-2/3 flex flex-col items-center justify-center">
+          <div className="w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20">
+            <span className="flex gap1 py-1 px-3 border rounded-full text-sm md:text-base border-gray-300 text-gray-600"></span>
+          </div>
+        </div>
+        {/* right side */}
+        <div ></div>
+      </div>
 
     </div>
   );
