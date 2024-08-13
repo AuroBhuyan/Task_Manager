6ed20df9c1df3a9d140c1eafc91e38ef7b2a16c9
AuroBhuyan <aurobhuyan123@gmail.com>
2024-08-13 15:19:56 +0530

    a

--- client/src/components/UserAvatar.jsx
+++ client/src/components/UserAvatar.jsx
@@ -29,7 +29,6 @@ const UserAvatar = () => {
             </Menu.Button>
           </div>
 
-
           <transition
             as={Fragment}
             enter-active-class="transition duration-100 ease-out"
@@ -39,7 +38,45 @@ const UserAvatar = () => {
             leave-from-class="transform scale-100 opacity-100"
             leave-to-class="transform scale-95 opacity-0"
           >
+            <Menu.Items className='absolute right-0 mt-2 w-56 origin-top-right divide-gray-100 rounded-md bg-white shadow-2xl ring-1 ring-black/5 focus:outline-none'>
+              <div className='p-4'>
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      onClick={() => setOpen(true)}
+                      className='text-gray-700 group flex w-full items-center rounded-md px-2 py-2 text-base'
+                    >
+                      <FaUser className='mr-2' aria-hidden='true' />
+                      Profile
+                    </button>
+                  )}
+                </Menu.Item>  
+        
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      onClick={() => setOpenPassword(true)}
+                      className={`tetx-gray-700 group flex w-full items-center rounded-md px-2 py-2 text-base`}
+                    >
+                      <FaUserLock className='mr-2' aria-hidden='true' />
+                      Change Password
+                    </button>
+                  )}
+                </Menu.Item>
 
+                <Menu.Item>
+                  {({ active }) => (
+                    <button
+                      onClick={logoutHandler}
+                      className={`text-red-600 group flex w-full items-center rounded-md px-2 py-2 text-base`}
+                    >
+                      <IoLogOutOutline className='mr-2' aria-hidden='true' />
+                      Logout
+                    </button>
+                  )}
+                </Menu.Item>
+              </div>
+            </Menu.Items>
         </transition>
         </Menu>
       </div>
