8cfedd6b2a82681fc21fb237a7ca543d8b9c086c
AuroBhuyan <aurobhuyan123@gmail.com>
2024-08-16 03:19:50 +0530

    a

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -30,11 +30,11 @@ const Login = () => {
         <div className='h-full w-full lg:w-2/3 flex flex-col items-center justify-center'>
           <div className='w-full md:max-w-lg 2xl:max-w-3xl flex flex-col items-center justify-center gap-5 md:gap-y-10 2xl:-mt-20'>
             <span className='flex gap1 py-1 px-3 border rounded-full text-sm md:text-base border-gray-300 text-gray-600'>
-              BUD
+              Manage all your tasks in one place!
             </span>
             <p className='flex flex-col gap-0 md:gap-4 text-4xl md:text-6xl 2xl:text-7xl font-black text-center text-custom-color'>
-              <span>Onii Chan</span>
-              <span>Baka!!!</span>
+              <span>Cloud-Based</span>
+              <span>Task Manager</span>
             </p>
 
             <div className='cell'>
