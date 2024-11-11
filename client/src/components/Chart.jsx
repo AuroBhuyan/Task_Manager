8e10f5f8d92ea821f45cea8b665e9e7d1345cea7
AuroBhuyan <aurobhuyan123@gmail.com>
2024-11-12 04:36:49 +0530

    a

--- client/src/components/Chart.jsx
+++ client/src/components/Chart.jsx
@@ -24,3 +24,4 @@ export const Chart = ({ data }) => {
     </ResponsiveContainer>
   );
 };
+ 
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -14,6 +14,8 @@ import clsx from "clsx";
 import { Chart } from "../components/Chart";
 import { BGS, PRIOTITYSTYELS, TASK_TYPE, getInitials } from "../utils";
 import UserInfo from "../components/UserInfo";
+import { useGetDashboardStatsQuery } from "../redux/slices/api/taskApiSlice";
+import Loading from "../components/Loader";
 
 const TaskTable = ({ tasks }) => {
   const ICONS = {
@@ -145,27 +147,22 @@ const UserTable = ({ users }) => {
 };
 
 const Dashboard = () => {
-  const [data, setData] = useState({
-    graphData: [],
-    last10Task: [],
-  });
-
-  useEffect(() => {
-    // You can replace this with fetching data from an API or other sources.
-    const fetchedData = {
-      graphData: summary.graphData || [], // Example data
-      last10Task: summary.last10Task || [], // Example data
-    };
-    setData(fetchedData);
-  }, []);
-
-  const totals = summary.tasks;
+  const {data,isLoading } = useGetDashboardStatsQuery();
+  if(isLoading){
+    return(
+      <div className='py-10'>
+        <Loading/>
+        </div>
+    )
+  }
+ 
+  const totals = data?.tasks;
 
   const stats = [
     {
       _id: "1",
       label: "TOTAL TASK",
-      total: summary?.totalTasks || 0,
+      total: data?.totalTasks || 0,
       icon: <FaNewspaper />,
       bg: "bg-[#1d4ed8]",
     },
@@ -225,7 +222,7 @@ const Dashboard = () => {
         <h4 className='text-xl text-gray-600 font-semibold'>
           Chart by Priority
         </h4>
-        <Chart data={data.graphData} />
+        <Chart data={data?.graphData} />
       </div>
 
       <div className='w-full flex flex-col md:flex-row gap-4 2xl:gap-10 py-8'>
